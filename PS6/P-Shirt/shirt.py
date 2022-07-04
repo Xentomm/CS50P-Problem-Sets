34b654bac9dec3d846c9a45b55d1b75bb8644159
Łukasz Erimus <108549372+Xentomm@users.noreply.github.com>
2022-07-04 11:48:47 +0200

    Add files via upload

---
+++ PS6/P-Shirt/shirt.py
@@ -0,0 +1,43 @@
+import sys
+from os.path import splitext
+from PIL import Image, ImageOps
+
+def main():
+    check_command_line_arg()
+
+    try:
+        imagefile = Image.open(sys.argv[1])
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+
+    shirtfile = Image.open("shirt.png")
+    size = shirtfile.size
+    print(size)
+    muppet = ImageOps.fit(imagefile, size)
+    muppet.paste(shirtfile, shirtfile)
+    muppet.save(sys.argv[2])
+
+def check_command_line_arg():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    file1 = splitext(sys.argv[1])
+    file2 = splitext(sys.argv[2])
+
+    if check_extention(file1[1]) == False:
+        sys.exit("Invalid Input")
+    if check_extention(file2[1]) == False:
+        sys.exit("Invalid Output")
+
+    if file1[1].lower() != file2[1].lower():
+        sys.exit("Input and output have different extensions")
+
+def check_extention(file):
+    if file in [".jpg", ".jpeg", ".png"]:
+        return True
+    return False
+
+if __name__ == "__main__":
+    main()
